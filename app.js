29e3d5e64d21fbea591898c7e74fcffe184290c0
skillza-quest <105557961+skillza-quest@users.noreply.github.com>
2023-09-25 10:39:06 +0400

    https removed

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const defaultRoutes = require('./routes/defaultRoutes');
+
 const investorRoutes = require('./routes/investorRoutes');
 const companyRoutes = require('./routes/companyRoutes');
 const userRoutes = require('./routes/userRoutes');
-const fs = require('fs'); // Add this line for working with file system
 const https = require('https');
 const app = express();
 
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 // Enable CORS only for a specific origin
 app.use(cors({
-  origin: 'http://localhost:3000'  // Replace with your application's actual address
+  origin: 'http://localhost:3000'  // replace with your application's actual address
 }));
 
 // MongoDB Atlas connection
@@ -28,17 +28,8 @@ app.use('/investors', investorRoutes);
 app.use('/users', userRoutes);
 app.use('/companies', companyRoutes);
 
-// HTTPS Configuration
-const privateKey = fs.readFileSync('path/to/your/private-key.pem', 'utf8'); // Replace with the actual path to your private key file
-const certificate = fs.readFileSync('path/to/your/certificate.pem', 'utf8'); // Replace with the actual path to your certificate file
-
-const credentials = { key: privateKey, cert: certificate };
-
-// Create an HTTPS server
-const httpsServer = https.createServer(credentials, app);
-
-// Start the HTTPS server
+// Start the Server
 const PORT = 3001;
-httpsServer.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
